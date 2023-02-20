2dc31722d03976f9f5e9c79b63dafebbb23c292e
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 13:10:22 +0530

    added: logic for getting total wage

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10EmpWage
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRate;
+        public int workingDays;
+        public int maxHours;
+        public int totalWage;
+
+        public CompanyEmpWage(string company, int empRate, int workingDays, int maxHours)
+        {
+            this.company = company;
+            this.empRate = empRate;
+            this.workingDays = workingDays;
+            this.maxHours = maxHours;
+        }
+
+        public void setTotalWage(int totalWage)
+        {
+            this.totalWage = totalWage;
+        }
+
+        public string toString()
+        {
+            return "Total Employee wage for company " + this.company + " is: " + this.totalWage;
+        }
+    }
+
+
+}
