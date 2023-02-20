843ce961f80d357f458b6465fc6a5ac9ce45900c
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 11:58:18 +0530

    added: greeting message for homepage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day10EmpWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("UC_10 Solution -- Check wage for multiple companies");
+        }
+    }
+}
