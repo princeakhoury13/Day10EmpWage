822757dd01b625f24b12d97cad6d7ba8882cc087
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-21 10:38:52 +0530

    added: New interface for Emp Wage

---
+++ IEmpWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10EmpWage
+{
+    public interface IEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRate, int workingDays, int maxHours);
+        public void computeEmpWage();
+        
+    }
+}
